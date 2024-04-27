b7c5158b61bfd1fcd2ca7ecaf0dab5b8daf28d35
Muhibbulloh <166902979+muhibprogg@users.noreply.github.com>
2024-04-27 15:04:00 +0500

    Make one round play function

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ function getHumanChoice() {
 
 let computerScore = 0;
 let humanScore = 0;
+let winner;
+
+function playRound(humanChoice, computerChoice) {
+  if (computerChoice === "rock" && humanChoice === "scissors") {
+    computerScore++;
+    winner = "computer";
+  } else if (computerChoice === "paper" && humanChoice === "rock") {
+    computerScore++;
+    winner = "computer";
+  } else if (computerChoice === "scissors" && humanChoice === "paper") {
+    computerScore++;
+    winner = "computer";
+  } else if (computerChoice === humanChoice) {
+    console.log(`It's a tie!`);
+    winner = "tie";
+  } else {
+    humanScore++;
+    winner = "human";
+  }
+  console.log(`You chose ${humanChoice}. Computer chose ${computerChoice}.`);
+  const msg =
+    winner === "tie" ? "No one wins! It's a tie." : `Winner is ${winner}.`;
+  console.log(msg);
+  console.log(`Score ->  (Human) ${humanScore} : ${computerScore} (Computer)`);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
