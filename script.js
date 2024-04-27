f2d0cce14c29f77040250051a35447462026c3cc
Muhibbulloh <166902979+muhibprogg@users.noreply.github.com>
2024-04-27 11:21:08 +0500

    Add get human choice function

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function getComputerChoice() {
   const index = Math.floor(Math.random() * choices.length);
   return choices[index];
 }
+
+function getHumanChoice() {
+  const choice = prompt("Rock, paper or scissors? ").toLowerCase();
+  return choice;
+}
